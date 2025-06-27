d772dc758ed4f794a5852fe8c9b9a7c103056194
mirayatech <mirayaabrodi@gmail.com>
2025-06-27 10:18:11 +0200

    feat: add TabNavigation index file to export components for improved modularity and accessibility

---
+++ src/components/TabNavigation/index.ts
@@ -0,0 +1,6 @@
+export { default as TabNavigation } from "./TabNavigation";
+export { ScrollControls } from "./ScrollControls";
+export { SortableTabItem } from "./SortableTabItem";
+export { TabSeparator } from "./TabSeparator";
+export { AddPageButton } from "./AddPageButton";
+export { DragOverlayTab } from "./DragOverlayTab";
